4726ed11145756475671ff1d1f10e355e0cec3fe
FaisalLou <faisallourenco27@gmail.com>
2025-11-25 11:41:28 +0000

    update lesson data transformation to use 'id' instead of '_id'

--- lesson_routes.js
+++ lesson_routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
         
         // Transform the data to match frontend expectations
         const transformedLessons = lessons.map(lesson => ({
-            _id: lesson._id,
+            id: lesson.id,
             topic: lesson.topic,
             location: lesson.location,
             price: lesson.price,
